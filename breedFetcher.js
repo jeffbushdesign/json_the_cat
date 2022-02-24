73c74caa83984c2ae3ef84912008aa2e8f80522f
Jeff Bush <jeffbushdesign@gmail.com>
2022-02-24 11:09:40 -0500

    Minor change

--- breedFetcher.js
+++ breedFetcher.js
@@ -15,16 +15,11 @@ request(`${search}`, (error, response, body) => {
   if (error) {
     console.log('error:', error); // Print the error if one occurred
   }
-  // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  // console.log('body:', body); // Print the HTML 
   if (data.length < 1) {
     console.log("Sorry, we can't find that breed of cat.\nPlease try another breed.");
   } else {
     console.log(data[0]['description']);
   }
-  // console.log(data);
-  // console.log(typeof data);
-  // console.log(data[0]['name']);
 });
 
 // How to use this code
